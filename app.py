106348f561acb4dadc25cf1006df84637e7a2c19
dev039211 <108935683+dev039211@users.noreply.github.com>
2024-10-09 00:01:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pickle
-import tensorflow as tf
+#import tensorflow as tf
 #print(tf.__version__)
 from tensorflow.keras.models import load_model
 #from tensorflow.keras.models import load_model, save_mod
