f54b10829e5945c38166665c4f22f1b003ad204e
dev039211 <108935683+dev039211@users.noreply.github.com>
2024-10-11 00:25:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
 import pickle
-#import tensorflow as tf
-#print(tf.__version__)
 from tensorflow.keras.models import load_model
-#from tensorflow.keras.models import load_model, save_mod
 from tensorflow.keras.preprocessing import sequence
 
 from flask import Flask, render_template, request
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
-#missing part
 analyzer = SentimentIntensityAnalyzer()
 
-# Global variables for model and tokenizer
 model = None
 tokenizer = None
 
 def load_keras_model():
     global model
-    # Load the model using the SavedModel format for better compatibility
-    #model = load_model('models/uci_sentimentanalysis.keras')
     model = load_model('models/uci_sentimentanalysis.h5')
 
 def load_tokenizer():
@@ -36,8 +29,7 @@ def sentiment_analysis(input):
     user_sequences = tokenizer.texts_to_sequences([input])
     user_sequences_matrix = sequence.pad_sequences(user_sequences, maxlen=1225)
     prediction = model.predict(user_sequences_matrix)
-    #return round(float(prediction[0][0]), 2)
-    return round(float(prediction[0][0]) * 100, 2)
+    return round(float(prediction[0][0]),2)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -45,7 +37,8 @@ def index():
     text = ""
     if request.method == "POST":
         text = request.form.get("user_text")
-        sentiment = analyzer.polarity_scores(text)
+        sentiment = analyzer.polarity_scores(text) # VADER results
+        # create a new key in the dictionary to store the custom model sentiment analysis results
         sentiment["custom model positive"] = sentiment_analysis(text)
     return render_template('form.html', sentiment=sentiment, user_text=text)
 
